c963c8882328bfd28fde210b6d62276c2dfa3dac
shyamg090 <samshyam9630@gmail.com>
2024-07-27 10:12:21 +0530

    Building food recipie app

---
+++ src/components/pages/Error.js
@@ -0,0 +1,11 @@
+const Error = () =>{
+    return (
+        <>
+        <h1>
+            this page doesnt exist
+        </h1>
+        </>
+    )
+}
+
+export default Error;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from './components/App'
+import About from './components/pages/About'
+import Contact from './components/pages/Contact';
+import Error from './components/pages/Error';
+
+const appLayout = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        errorElement : <Error/>
+    },
+    {
+        path: '/about',
+        element: <About />
+    },
+    {
+        path : 'contact',
+        element : <Contact/>
+    }
+])
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
 root.render(
-    <App />
+    <RouterProvider router={appLayout} />
 );
